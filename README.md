67e3ed24655b301340b081aebcfa3f57409b5169
Nathan Perez <107879578+nateeatbacon@users.noreply.github.com>
2022-06-20 19:03:25 +0000

    a lot

---
+++ README.md
@@ -0,0 +1,17 @@
+## Input
+Input is the way the computer gets infromation from the user 
+
+Notes: Python initially treats input as a `string` type
+
+Ex.
+
+5-->"5"
+
+Today is Monday --> :"Today is Monday"
+
+### Formula
+`varibaleName = input("PROMPT")
+
+PROMPT - A question or statement that the computer asks the user
+
+Python stores the __input__ in the __variableName__
---
+++ main.py
@@ -0,0 +1,32 @@
+'''
+userName = input("Enter your name: ")
+print("Hey " + userName)
+
+userGrade = input("What grade are you in? ")
+print("Hey " + userName + ", " + userGrade + " is a lame grade")
+
+userYear = int(input("What year were you born? "))
+userAge = 2022 - userYear
+print(str(userAge) + " is so young!")
+'''
+
+#Create your own Madlips
+# Have at least 5 inputs
+noun = input("Pleas give me a noun: " )
+noun1 = input("Pleas give me a noun: " )
+noun2 = input("Pleas give me a noun: " )
+noun3 = input("Pleas give me a noun: " )
+noun4 = input("Pleas give me a noun: " )
+noun5 = input("Pleas give me a noun: " )
+verb = input("Pleas give me a verb: ")
+noun6 = input("Pleas give me a noun: " )
+noun7 = input("Pleas give me a noun: " )
+noun8 = input("Pleas give me a noun: " )
+verb2 = input("Pleas give me a verb: ")
+noun9 = input("Pleas give me a noun: ")
+noun10 = input("Pleas give me a noun: " )
+adjective = input("Pleas give me a adjective: ")
+adjective = input("Pleas give me a adjective: ")
+noun,place = input("Pleas give me a noun,place: ")
+
+print("")
