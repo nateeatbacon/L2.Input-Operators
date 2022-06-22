f768610521f797fd2b87ffcb8646d53ce7be0927
Nathan Perez <107879578+nateeatbacon@users.noreply.github.com>
2022-06-22 17:07:29 +0000

    note note nootw

--- README.md
+++ README.md
@@ -15,3 +15,52 @@ Today is Monday --> :"Today is Monday"
 PROMPT - A question or statement that the computer asks the user
 
 Python stores the __input__ in the __variableName__
+
+## Operators
+## Arithmetic Operators
+use to peform
+mathematic operations
+
+1. Addition `+`
+2. Subtraction `-`
+3. Multiplications `*`
+4. Divison `/` (Exact Answer),`//` (Quotient Divison)
+5. Modulo `%` (Remainder)
+6. Exponent `**`
+
+Ex.
+```python # Code Black
+print(5 / 2) #2.5
+print( 5 // 2) #2
+print(5 % 2) #1
+print(4 ** 2) #16
+```
+### Assigment Operators
+
+1. `=` Simple Assignment
+2. `+=` Addition Assigment
+3. `-+` Subtraction Assigment
+4. `*=`, `/=`, `//=`, `**=`, `%=`
+
+Ex.
+```python
+
+a = 2
+a += 3 # 5, a = a + 3
+a = 1 
+a = 3 # 3
+```
+
+### Comparsion Operators
+
+1. `==` equal to
+2. `>=` greator than or equal to
+3. `>` greater than
+4. `<=` les than or equal to
+5. `<` less than
+6. `!=` not equal to
+
+### Boolean Operators
+1. and (PRECDEDENCE)
+2. or
+3. not
--- main.py
+++ main.py
@@ -12,22 +12,18 @@ print(str(userAge) + " is so young!")
 
 #Create your own Madlips
 # Have at least 5 inputs
-noun = input("Pleas give me a noun: " )
-noun1 = input("Pleas give me a noun: " )
+animal = input("Pleas give me a animal: " )
+person1 = input("Pleas give me a person: " )
 noun2 = input("Pleas give me a noun: " )
 noun3 = input("Pleas give me a noun: " )
 noun4 = input("Pleas give me a noun: " )
 nameADisaster = input("Pleas give me a disaster: " )
 person = input("Pleas give me a person: ")
-noun6 = input("Pleas give me a noun: " )
-noun7 = input("Pleas give me a noun: " )
-noun8 = input("Pleas give me a noun: " )
-verb2 = input("Pleas give me a verb: ")
-noun9 = input("Pleas give me a noun: ")
-noun10 = input("Pleas give me a noun: " )#adjective = input("Pleas give me a adjective: ")
-adjective = input("Pleas give me a adjective: ")
-place = input("Pleas give me a noun,place: ")
+place = input("Pleas give me a place: " )
+somthingYouHate = input("Pleas give me omthing you hate: ")
+wepon = input("pleas give me a wepon: ")
 
-print(noun + " and "+noun1 + " wanted to go camping. They neeed a cabin. They decided rent a cabin out of "+noun2 + "and "+noun3+". The next the packed there othere things they would need like "+noun4+". But then "+nameADisaster+ " happends and kills "+person+ ".
+
+print("animal" + " and" +person1 +  "wanted to go camping. They neeed a cabin. They decided rent a cabin out of a "+noun2 + " and "+noun3+". The next the packed there othere things they would need like "+noun4+". But then "+nameADisaster+ " happends and kills "+person+ ". You start to cry because you loved this person.You decide to go to the "+place+ ". You are safe there but intill a "+somthingYouHate+ " attacks. You decide to fight it with a "+wepon+ ".You kill it and you are a hero nice job.")
 
       
